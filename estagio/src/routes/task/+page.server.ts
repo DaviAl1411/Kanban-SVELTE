fac76b4c1d1acedb414d8c1b7e68243cc7e7a255
Davi Almeida <davi.al1411@gmail.com>
2026-09-22 09:23:14 -0300

    Adicionando filtro para pesquisas

--- estagio/src/routes/task/+page.server.ts
+++ estagio/src/routes/task/+page.server.ts
@@ -1,8 +1,22 @@
 import type { PageServerLoad } from "./$types";
 import { tasks } from "$lib/data/mock";
 
-export const load: PageServerLoad = () => {
+export const load: PageServerLoad = ({url}) => {
+    const status = url.searchParams.get("status")
+    const priority = url.searchParams.get("priority")
+
+    const tasksStatus = status
+        ? tasks.filter((task) => task.status === status)
+        : tasks;
+    
+    const tasksFiltradas = priority
+        ? tasksStatus.filter((task) => task.priority === priority)
+        : tasksStatus;
+     
+
     return {
-        tasks
+        tasks: tasksFiltradas,
+        status,
+        priority
     }
 }
--- estagio/src/routes/task/+page.svelte
+++ estagio/src/routes/task/+page.svelte
@@ -5,6 +5,27 @@
 
 <h1>Tasks</h1>
 
+
+<form method="GET">
+    <select name="status" value= {data.status ?? ""}>
+        <option value="">Todas</option>
+        <option value="TODO">A fazer</option>
+        <option value="DOING">Em andamento</option>
+        <option value="DONE">Concluída</option>
+    </select>
+
+    <button type="submit">Filtrar Status</button>
+
+    <select name="priority" value= {data.priority ?? ""}>
+        <option value="">Todas</option>
+        <option value="LOW">Baixa</option>
+        <option value="MEDIUM">Média</option>
+        <option value="HIGH">Alta</option>
+    </select>
+
+    <button type="submit">Filtrar Prioridade</button>
+</form>
+
 {#each data.tasks as task}
     <Task {task} />
 {/each}
